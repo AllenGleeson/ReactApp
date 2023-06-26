187a4ea41e25861ea4c687406423eea4141cda32
AllenGleeson <allen_gleeson@hotmail.com>
2023-06-26 22:52:10 +0100

    Creating header and about me section.

---
+++ src/Components/Greeting.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Greeting = () => {
+    return React.createElement('h1', {id: 'heading', className: 'bg-dark text-white'}, 'My man');
+}
+
+export default Greeting
