fe865543ced6141034a4f0f07537f58f9991b782
AllenGleeson <allen_gleeson@hotmail.com>
2023-06-29 22:57:43 +0100

    Working out routes, begining to create Shop component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React, {Component} from 'react';
-import {  Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutMe from './Components/AboutMe';
 import Content from './Components/Content';
-import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import RootLayout from './Layouts/RootLayout';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, Router, Route, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
-      <Route path='/about' element={AboutMe} />
-      <Route path='/content' element={Content} />
-    </Route>
+      <Route>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/about" element={<Content />} />
+      </Route>
   )
 )
 
-class App extends Component{
-  render(){
-    return(
-      <RouterProvider router={router} />
-    );
-  }
-}
+function App(){
+  return(
+    <RouterProvider router={router} />
+  );
+};
+
 
 export default App;
---
+++ src/Components/Shop.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+const Shop = () => {
+    return React.createElement('section', { className: 'about-me-section' },
+        React.createElement('div', { className: 'row' },
+        React.createElement('div', { className: 'col-12 col-md-6' },
+            React.createElement('img', { className: 'w-100', src: 'https://cdn.dribbble.com/userupload/3158902/file/original-7c71bfa677e61dea61bc2acd59158d32.jpg?resize=400x0', alt: 'Image description' })
+        ),
+        React.createElement('div', { className: 'col-12 col-md-6 position-relative about-me-content' },
+            React.createElement('article', { className: 'text-dark text-center' },
+            React.createElement('h2', null, 'Lorem Ipsum'),
+            React.createElement('p', null, articleContent)
+            ),
+            React.createElement('div', { className: 'bg-image' })
+        )
+        )
+    );
+}
+      
+      
+      
+
+export default Shop
