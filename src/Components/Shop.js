20f2200453af921b748957c84d535f3fb36c6bd3
AllenGleeson <allen_gleeson@hotmail.com>
2023-07-05 23:31:58 +0100

    Adding an icon to each item and a dropdown when they click that item for more information about a project.

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -2,26 +2,35 @@ import React from 'react'
 import { motion } from 'framer-motion';
 import images from './images';
 import '../css/FeaturedProjects.css';
-import { faCircleInfo } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook, faFaceKiss } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
 
 
 
 const Shop = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
+
     return (
         <motion.div className='carousel'>
             <motion.div className='inner-carousel'>
                 {images.map((image) => {
                     return(
                         <motion.div drag="x" dragConstraints={{ left: 0, right: 300 }} className='item'>
-                            <img src={image}>
-                            <FontAwesomeIcon icon="fa-regular fa-circle-info" beat size="2xs" />
-                            </img>
+                            <img src={image} />
+                            <FontAwesomeIcon icon={isOpen ? faAddressBook : faFaceKiss} beat size="lg" onClick={toggleDropdown} className='test' />
+                            {isOpen && (
                             <aside className='project-information'>
                                 <h2>Project</h2>
                                 <a href='#'>GitHub</a>
                                 <a href='#'>Live</a>
                                 <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
                             </aside>
+                            )}
                         </motion.div>
                     );
                 })}
@@ -30,7 +39,26 @@ const Shop = () => {
     );
 }
       
-      
+// const Dropdown = () => {
+//     const [isOpen, setIsOpen] = useState(false);
+  
+//     const toggleDropdown = () => {
+//       setIsOpen(!isOpen);
+//     };
+  
+//     return (
+//       <div className="dropdown">
+//         <div className="dropdown-toggle" onClick={toggleDropdown}>
+//           <FontAwesomeIcon icon={isOpen ? faAngleUp : faAngleDown} />
+//         </div>
+//         {isOpen && (
+//           <div className="dropdown-content">
+//             {/* Additional information goes here */}
+//           </div>
+//         )}
+//       </div>
+//     );
+//   };      
       
 
 export default Shop
