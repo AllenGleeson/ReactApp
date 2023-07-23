c34069dbe926f70c1a97d68682b78ae6e1c1bcce
AllenGleeson <allen_gleeson@hotmail.com>
2023-07-23 22:41:48 +0100

    Minor refactoring.

--- src/features/projects/components/card.js
+++ src/features/projects/components/card.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import '../../../assets/css/card.css';
 
-let articleContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam venenatis erat id felis porttitor,'+
- 'et vestibulum dui euismod. Aenean in velit et urna mollis volutpat nec quis urna. Sed eu lorem nisl.'+
-  'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'+
-   'Quisque quis ligula vehicula, auctor metus tristique, eleifend velit. Duis ac magna magna.'+
-    'Nulla sed commodo magna, et feugiat libero. Cras bibendum luctus justo. Nulla at suscipit neque.'
 
 const Card = ({ index, title, description, technologies }) => {
-    const isEven = (index+1) % 2 === 0;
-    const cardClassName = `mb-3 col-sm-10 col-md-5 p-2 ${isEven ? "offset-md-1" : "offset-md-1"}`;
-
 
     return (
-        <div className={cardClassName}>
+        <div className="mb-3 col-sm-10 col-md-5 p-2 offset-md-1">
             <div class="card container">
-                <div class="row no-gutters">
+                <div class="row">
                     <div class="col-md-4 p-0">
                         <img src="https://preview.redd.it/r19fc58otvn31.jpg?width=640&crop=smart&auto=webp&s=7956096bfcd6c77e7873f1728d9759869485c5fd" class="card-img" alt="Project One" />
                     </div>
