faf7a373ecdf7a393771e1019022beda6c86b8cf
AllenGleeson <allen_gleeson@hotmail.com>
2023-07-25 01:57:36 +0100

    Added some on hover css to project cards and logo.

--- src/features/projects/components/cards.js
+++ src/features/projects/components/cards.js
@@ -6,32 +6,38 @@ const Cards = () => {
     const projects = [
         {
           name: "Project 1",
-          description: "Description of Project 1",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."+""+
+          "Morbi et ultricies lectus. Proin vel maximus est. Aliquam sollicitudin mauris quis mauris venenatis, a sagittis elit volutpat.",
           technologies: ["React", "JavaScript", "CSS"],
         },
         {
           name: "Project 2",
-          description: "Description of Project 2",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."+""+
+          "Morbi et ultricies lectus. Proin vel maximus est. Aliquam sollicitudin mauris quis mauris venenatis, a sagittis elit volutpat.",
           technologies: ["React", "Node.js", "Express"],
         },
         {
           name: "Project 3",
-          description: "Description of Project 2",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."+""+
+          "Morbi et ultricies lectus. Proin vel maximus est. Aliquam sollicitudin mauris quis mauris venenatis, a sagittis elit volutpat.",
           technologies: ["React", "Node.js", "Express"],
         },
         {
           name: "Project 4",
-          description: "Description of Project 2",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."+""+
+          "Morbi et ultricies lectus. Proin vel maximus est. Aliquam sollicitudin mauris quis mauris venenatis, a sagittis elit volutpat.",
           technologies: ["React", "Node.js", "Express"],
         },
         {
           name: "Project 5",
-          description: "Description of Project 2",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."+""+
+          "Morbi et ultricies lectus. Proin vel maximus est. Aliquam sollicitudin mauris quis mauris venenatis, a sagittis elit volutpat.",
           technologies: ["React", "Node.js", "Express"],
         },
         {
           name: "Project 6",
-          description: "Description of Project 2",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."+""+
+          "Morbi et ultricies lectus. Proin vel maximus est. Aliquam sollicitudin mauris quis mauris venenatis, a sagittis elit volutpat.",
           technologies: ["React", "Node.js", "Express"],
         }
       ];
