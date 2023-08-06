d51476ad2d842d462cd679c4921264c5ef85766a
AllenGleeson <allen_gleeson@hotmail.com>
2023-08-06 23:54:56 +0100

    Changed highlight to take handle images from public dir. Mobile and Admin view buttons now show respective images.

--- src/features/projects/components/highlight.js
+++ src/features/projects/components/highlight.js
@@ -1,56 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../../../assets/css/project.css';
 
-
 const Highlight = ({ index, title, description, image, mobileVersion, admin }) => {
-    const isEven = index%2==0;
-    const highlightClass = `mt-3 mb-3 ${isEven == false ? "slideLeft dynamicTheme rounded-bottom-circle rounded-top-pill" : "alternateBg text-light slideRight"}`;
+    const [selectedView, setSelectedView] = useState('desktop');
+
+    const isEven = index % 2 === 0;
+    const highlightClass = `mt-3 mb-3 ${
+        isEven === false
+            ? 'slideLeft dynamicTheme rounded-bottom-circle rounded-top-pill'
+            : 'alternateBg text-light slideRight'
+    }`;
+
+    // Event handler for the "Mobile Version" or "Admin View" button click
+    const handleViewClick = (view) => {
+        setSelectedView((prevView) => (prevView === view ? 'desktop' : view));
+    };
+
+    const getImagePath = () => {
+        const baseImagePath = `${process.env.PUBLIC_URL}/data/images/`;
+        const baseFolder = selectedView === 'admin' ? 'desktop' : selectedView;
+        const isAdmin = selectedView === 'admin';
+        const viewFolder = isAdmin ? 'admin' : 'users';
+        return `${baseImagePath}${baseFolder}/${viewFolder}/${image}`;
+    };   
 
     return (
         <div className='col-12'>
             <div className={highlightClass}>
-            {(() => {
-                if(image!==null) {
-                    if(isEven){
-                        return (
-                            <div className='row'>
-                                <div className='col-sm-12 col-md-6'>
-                                    <div className='h-75 m-2 p-2 text-shadow'>
-                                        <div>
-                                            <h1>{title}</h1>
-                                            <hr />
-                                            <p>{description}</p>
-                                        </div>
-                                    </div>
-                                    {mobileVersion || admin ? (
-                                            <div className="h-25 d-flex">
-                                                {mobileVersion ? (
-                                                    <a href='#' className='text-decoration-none'>
-                                                        <span className='techTheme dynamicHover text-shadow-light-small p-1 m-2 rounded border border-dark-subtle'>Mobile Version</span>
-                                                    </a>
-                                                ) : null}
-                                                {admin ? (
-                                                    <a href='#' className='text-decoration-none'>
-                                                        <span className='techTheme dynamicHover text-shadow-light-small p-1 m-2 rounded border border-dark-subtle'>Admin View</span>
-                                                    </a>
-                                                ) : null}
-                                            </div>
-                                    ) : null}
-                                </div>
+                {image !== null ? (
+                    <div className='row'>
+                        {isEven ? (
+                            // Render for even index
+                            <>
                                 <div className='col-sm-12 col-md-6'>
                                     <div className='m-3'>
-                                        <img className='border border-4 border-dark rounded w-100'src={image}alt='Project' />
-                                    </div>
-                                </div>
-                            </div>  
-                        )
-                    }
-                    else{
-                        return (
-                            <div className='row'>
-                                <div className='col-sm-12 col-md-6'>
-                                    <div className='m-3'>
-                                        <img className='border border-4 border-dark rounded w-100'src={image}alt='Project' />
+                                        <img
+                                            className='border border-4 border-dark rounded w-100'
+                                            src={getImagePath()}
+                                            alt='Project'
+                                        />
                                     </div>
                                 </div>
                                 <div className='col-sm-12 col-md-6'>
@@ -59,45 +47,80 @@ const Highlight = ({ index, title, description, image, mobileVersion, admin }) =
                                         <hr />
                                         <p>{description}</p>
                                     </div>
-                                    {mobileVersion || admin ? (
-                                            <div className="h-25 d-flex justify-content-end">
-                                                {mobileVersion ? (
-                                                    <a href='#' className='text-decoration-none'>
-                                                        <span className='techTheme dynamicHover text-shadow-light-small p-1 m-2 rounded border border-dark-subtle'>Mobile Version</span>
-                                                    </a>
-                                                ) : null}
-                                                {admin ? (
-                                                    <a href='#' className='text-decoration-none'>
-                                                        <span className='techTheme dynamicHover text-shadow-light-small p-1 m-2 rounded border border-dark-subtle'>Admin View</span>
-                                                    </a>
-                                                ) : null}
-                                            </div>
-                                    ) : null}
+                                    {(mobileVersion || admin) && (
+                                        <div className='d-flex h-25'>
+                                            {mobileVersion && (
+                                                <span onClick={() => handleViewClick('mobile')} className='techTheme dynamicHover text-shadow-light-small p-1 m-2 rounded border border-dark-subtle'>
+                                                    {selectedView === 'mobile'
+                                                        ? 'Desktop Version'
+                                                        : 'Mobile Version'}
+                                                </span>
+                                            )}
+                                            {admin && (
+                                                <span onClick={() => handleViewClick('admin')} className='techTheme dynamicHover text-shadow-light-small p-1 m-2 rounded border border-dark-subtle'>
+                                                    {selectedView === 'admin'
+                                                        ? 'User View'
+                                                        : 'Admin View'}
+                                                </span>
+                                            )}
+                                        </div>
+                                    )}
                                 </div>
-                            </div>
-                        )
-                    }
-                }
-                else{
-                    return (
-                        <div className='row'>
-                            <div className='col-sm-12'>
-                                <div className='mt-3 p-3 text-center'>
-                                    <h1>{title}</h1>
-                                    <hr />
-                                    <p>{description}</p>
+                            </>
+                        ) : (
+                            // Render for odd index
+                            <>
+                                <div className='col-sm-12 col-md-6'>
+                                    <div className='h-75 m-2 p-2 text-shadow'>
+                                        <h1>{title}</h1>
+                                        <hr />
+                                        <p>{description}</p>
+                                    </div>
+                                    {(mobileVersion || admin) && (
+                                        <div className='d-flex h-25'>
+                                            {mobileVersion && (
+                                                <span onClick={() => handleViewClick('mobile')} className='techTheme dynamicHover text-shadow-light-small p-1 m-2 rounded border border-dark-subtle'>
+                                                    {selectedView === 'mobile'
+                                                        ? 'Desktop Version'
+                                                        : 'Mobile Version'}
+                                                </span>
+                                            )}
+                                            {admin && (
+                                                <span onClick={() => handleViewClick('admin')} className='techTheme dynamicHover text-shadow-light-small p-1 m-2 rounded border border-dark-subtle'>
+                                                    {selectedView === 'admin'
+                                                        ? 'User View'
+                                                        : 'Admin View'}
+                                                </span>
+                                            )}
+                                        </div>
+                                    )}
                                 </div>
+                                <div className='col-sm-12 col-md-6'>
+                                    <div className='m-3'>
+                                        <img
+                                            className='border border-4 border-dark rounded w-100'
+                                            src={getImagePath()}
+                                            alt='Project'
+                                        />
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                    </div>
+                ) : (
+                    <div className='row'>
+                        <div className='col-sm-12'>
+                            <div className='mt-3 p-3 text-center'>
+                                <h1>{title}</h1>
+                                <hr />
+                                <p>{description}</p>
                             </div>
                         </div>
-                    )
-                }
-            })()}
+                    </div>
+                )}
             </div>
         </div>
-    )
-}
-    
-      
-      
+    );
+};
 
 export default Highlight;
