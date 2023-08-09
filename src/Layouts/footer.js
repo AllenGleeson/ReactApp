28bbf5181d2c7b3d7a6d22196da64ae29eda7a0f
AllenGleeson <allen_gleeson@hotmail.com>
2023-08-09 21:37:12 +0100

    Added links to projects json

--- src/Layouts/footer.js
+++ src/Layouts/footer.js
@@ -9,7 +9,6 @@ const Footer = () => {
           <ul className="col-md-4 col-sm-6 footer-links offset-md-8">
             <li className='links'><a href='/'>HOME</a></li>
             <li className='links'><a href='/projects'>PROJECTS</a></li>
-            <li className='links'><a href='/contact'>CONTACT</a></li>
           </ul>
         </div>
       </div>
