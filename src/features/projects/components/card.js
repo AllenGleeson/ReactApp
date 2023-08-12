a93a09849148a4793be67050162adf8fa9d04c1d
AllenGleeson <allen_gleeson@hotmail.com>
2023-08-12 18:00:39 +0100

    Added Find Ireland images. Adjusted card to change if there is an overview image or not.

--- src/features/projects/components/card.js
+++ src/features/projects/components/card.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import '../../../assets/css/card.css';
 
-
-const Card = ({ index, title, description, links, image}) => {
-    const isEven = index%2==0;
+const Card = ({ index, title, description, links, image }) => {
+    const isEven = index % 2 === 0;
     const cardClass = `border border-bottom-0 border-end-0 card container tr-border-radius shadow-lg ${isEven ? "slideLeft" : "slideRight"}`;
-    const projectLink = '/projects/'+(index+1);
+    const projectLink = '/projects/' + (index + 1);
     const overviewImagePath = `${process.env.PUBLIC_URL}/data/images/overview/${image}`;
+
     return (
         <div className="mb-3 col-sm-10 col-md-5 p-2 offset-md-1">
             <div className={cardClass}>
                 <a href={projectLink} className="row">
-                    <div className="col-sm-12 col-md-4 p-0">
-                        <img src={overviewImagePath} className="card-img" alt="Project One" />
-                    </div>
-                    <div className="col-sm-12 col-md-8">
+                    {image && (
+                        <div className="col-sm-12 col-md-4 p-0">
+                            <img src={overviewImagePath} className="card-img" alt="Project One" />
+                        </div>
+                    )}
+                    <div className={image ? "col-sm-12 col-md-8" : "col-sm-12"}>
                         <div className="card-body tr-border-radius text-shadow">
                             <h5 className="card-title">{title}</h5>
                             <hr />
@@ -30,22 +32,18 @@ const Card = ({ index, title, description, links, image}) => {
                     </div>
                 </a>
             </div>
-            {index % 1 === 0 ? (
-            <div className="col-12 d-sm-none mb-5">
-                <hr />
-            </div>
-            ) : null}
-            {index % 2 === 0 ? (
-            <div className="col-12 d-none d-sm-block d-md-block d-lg-none mb-5">
-                <hr />
-            </div>
-            ) : null}
+            {index % 1 === 0 && (
+                <div className="col-12 d-sm-none mb-5">
+                    <hr />
+                </div>
+            )}
+            {index % 2 === 0 && (
+                <div className="col-12 d-none d-sm-block d-md-block d-lg-none mb-5">
+                    <hr />
+                </div>
+            )}
         </div>
-        
     )
 }
-      
-      
-      
 
 export default Card
