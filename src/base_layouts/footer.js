eef1320aa18977729a3f326235cd44aa5a5e765f
AllenGleeson <allen_gleeson@hotmail.com>
2023-09-02 19:33:15 +0100

    Removed unused files. Added custom scroll bar and new logo.

--- src/base_layouts/footer.js
+++ src/base_layouts/footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className='mt-4'>
       <div className='container'>
-        <div className="footer-content pt-3 row">
-          <ul className="col-md-4 col-sm-6 footer-links offset-md-8">
+        <div className="footer-content pb-2 pt-3">
+          <ul className="footer-links">
             <li className='nav-link p-1'><a href='/'>HOME</a></li>
             <li className='ms-3 nav-link p-1'><a href='/projects'>PROJECTS</a></li>
           </ul>
--- src/base_layouts/header.js
+++ src/base_layouts/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Navbar, Nav, NavItem, NavLink } from 'react-bootstrap';
 import '../assets/css/nav.css';
-import logo from '../assets/images/Logo.png';
+import logo from '../assets/images/page-logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
